4eb28c49fd0afcc865e75c5020087a3c8c6217e3
Even <even@kvenna.net>
2015-11-19 21:34:56 +0100

    Added WD reset

--- fryserKontroll/fryserKontroll.ino
+++ fryserKontroll/fryserKontroll.ino
@@ -1,9 +1,31 @@
+#include <LiquidCrystal.h>
+#include <avr/wdt.h>
+
+// initialize the LiquidCrystal library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup() {
-  // put your setup code here, to run once:
-  // interrup timers for WD og sjekking?
+  // LCD setup
+  lcd.begin(16, 2);
+  lcd.print("Ute:");
+  lcd.setCursor(0, 1);
+  lcd.print("Kompressor:");
+
+  // Setup serial for status dump
+  Serial.begin(9600);
+  
+  // Setup for ~8S WDT reset
+  noInterrupts();
+  wdt_reset();
+  MCUSR &= ~bit(WDRF);                            //clear WDRF
+  WDTCSR |= bit(WDCE) | bit(WDE);                 //enable WDTCSR change
+  WDTCSR =  bit(WDIE) | bit(WDP3) | bit(WDP0);    //~8 sec
+  interrupts();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  // Keep WD from barking!
+  wdt_reset();
 
 }
