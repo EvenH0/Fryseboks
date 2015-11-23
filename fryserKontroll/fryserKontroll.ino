075c23899eb007bc76eaee2684ff37769234d2ff
Even <even@kvenna.net>
2015-11-23 22:03:28 +0100

    Added libs

--- fryserKontroll/fryserKontroll.ino
+++ fryserKontroll/fryserKontroll.ino
@@ -1,36 +1,76 @@
+#include <DallasTemperature.h>
 #include <LiquidCrystal.h>
 #include <avr/wdt.h>
 
 // Constants
-//@ TODO: update with correct pins
-const int AMBIENT_TEMP_SERNSOR    =    0;
-const int COMPRESSOR_TEMP_SENSOR  =    0;
-const int FREEZER_TEMP_SENSOR     =    0;
-const int FREEZER_RELAY           =    9;
-const int HEATER_RELAY            =   10;
-const int REGULATOR_INTERVAL      = 3600;
+#define FREEZER_RELAY                  9
+#define HEATER_RELAY                  10
+#define REGULATOR_INTERVAL          3600
+#define ONE_WIRE_BUS                  13
+#define TEMPERATURE_PRECISION          9
 
-// Variables
+// Global Variables
 int ambientTemp                   =    0;
 int compressorTemp                =    0;
-int timing                        =    0;
+int freezerTemp                   =    0;
+int timer                         =    0;
+
 // initialize the LiquidCrystal library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device addresses
+DeviceAddress AMBIENT_TEMP_SERNSOR, COMPRESSOR_TEMP_SENSOR, FREEZER_TEMP_SENSOR;
 
 void setup() {
   // LCD setup
   lcd.begin(16, 2);
   lcd.print("Ute:");
+  lcd.setCursor(7, 0);
+  lcd.print("Boks:");
   lcd.setCursor(0, 1);
   lcd.print("Kompressor:");
 
   // Setup serial for status dump
   Serial.begin(9600);
 
+  // One Wire temp sensors
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  // Start up the library
+  sensors.begin();
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+  // report parasite power requirements
+  Serial.print("Parasite power is: "); 
+  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+  if (!sensors.getAddress(AMBIENT_TEMP_SERNSOR,   0)) Serial.println("Unable to find address for Device 0"); 
+  if (!sensors.getAddress(COMPRESSOR_TEMP_SENSOR, 1)) Serial.println("Unable to find address for Device 1"); 
+  if (!sensors.getAddress(FREEZER_TEMP_SENSOR,    2)) Serial.println("Unable to find address for Device 2"); 
+  // set the resolution to 9 bit
+  sensors.setResolution(AMBIENT_TEMP_SERNSOR,     TEMPERATURE_PRECISION);
+  sensors.setResolution(COMPRESSOR_TEMP_SENSOR,   TEMPERATURE_PRECISION);
+  sensors.setResolution(FREEZER_TEMP_SENSOR,      TEMPERATURE_PRECISION);
+  
   // Pin setup
-  pinMode( 9, OUTPUT);
-  pinMode(10, OUTPUT);
+  pinMode(FREEZER_RELAY, OUTPUT);
+  pinMode(HEATER_RELAY , OUTPUT);
+  
+  // Freezer ON
+  // N.C relay
+  digitalWrite(FREEZER_RELAY, HIGH);
+  
+  // Heater OFF
+  // N.O relay
+  digitalWrite(HEATER_RELAY, LOW);
   
   // Setup for ~8S WD reset
   noInterrupts();
@@ -40,6 +80,8 @@ void setup() {
   WDTCSR =  bit(WDIE) | bit(WDP3) | bit(WDP0);    //~8 sec
   interrupts();
 
+  Serial.println("Setup complete!");
+
 }
 
 void loop() 
@@ -48,37 +90,73 @@ void loop()
   wdt_reset();
 
   // Read temp
-  //@ TODO: get temp sensors
-  ambientTemp = 0;
-  compressorTemp = 0;
+  sensors.requestTemperatures();   
+  ambientTemp     = sensors.getTempC(AMBIENT_TEMP_SERNSOR);
+  compressorTemp  = sensors.getTempC(COMPRESSOR_TEMP_SENSOR);
+  freezerTemp     = sensors.getTempC(FREEZER_TEMP_SENSOR);
 
-  // 
+  // Regulate
+  if (timer >= REGULATOR_INTERVAL)
+  {
+    timer = 0;
+    freezerControl();
+  }
+  
+  // Update Display
+  //@ TODO: Limit decimals when printing
+  lcd.setCursor(4, 0);
+  lcd.print(ambientTemp);
+  lcd.setCursor(12, 0);
+  lcd.print(freezerTemp);
+  lcd.setCursor(12, 1);
+  lcd.print(compressorTemp);
+
+  // Hang a bit, get loop time ~1sec
+  delay(900);
+  ++timer;
+}
+
+void freezerControl()
+{
   if (ambientTemp <= -15)
   {
-    // Freezer relay is N.C
+    // It's cold outside turn off the freezer
+    // N.C relay
+    digitalWrite(FREEZER_RELAY, HIGH);
+  }
+  else if (ambientTemp <= -4 && freezerTemp <= -14)
+  {
+    // It's cold inside turn off the freezer
+    // N.C relay
     digitalWrite(FREEZER_RELAY, HIGH);
   }
   else 
   {
-    heaterControl();
+    // Need Heater?
+    // If heating wait 10min and try again
+    if (!heaterControl())
+    {
+      // It's time to FREEZE!
+      // N.C relay
+      digitalWrite(FREEZER_RELAY, LOW);
+    }
   }
-
-  // Update Display
-  lcd.setCursor(12, 0);
-  lcd.print(ambientTemp);
-  lcd.setCursor(12, 1);
-  lcd.print(compressorTemp);
-
-  // Hang a bit
-  delay(1000);
 }
 
-void heaterControl()
+bool heaterControl()
 {
-  ++timing;
-  if (timing >= REGULATOR_INTERVAL)
+  bool heating = false;
+  if (compressorTemp <= -4)
   {
-    
+    digitalWrite(HEATER_RELAY, HIGH);
+    heating = true;
+    // Heat for 10 min and check again.
+    timer = (REGULATOR_INTERVAL - 600);
   }
+  else
+  {
+    digitalWrite(HEATER_RELAY, LOW);
+  }
+  return heating;
 }
 
